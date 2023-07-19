965ff2efc93a4aad7414c72da65d7697a0be16dc
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2023-07-19 20:10:35 -0300

    Segunda aula Declarando variáveis

---
+++ conversoes.js
@@ -0,0 +1,11 @@
+// conversão implícita
+//const numero = 456;
+//const numeroString = "456";
+
+//console.log(numero == numeroString)
+
+// conversão explícita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + Number(numeroString))
---
+++ truthy-falsy.js
@@ -0,0 +1,25 @@
+// boolean
+const usuarioLogado = true
+const contaPaga = false
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// undefined
+// null
+
+let minhaVar;
+let varNull = null;
+
+console.log(typeof minhaVar)   
+console.log(typeof varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero)
+console.log(typeof texto)
